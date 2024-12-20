b6a5108e40b739131aebc615e5cdf9d866895e68
Abdulatif Tarek <138982485+Tifa200@users.noreply.github.com>
2024-12-20 17:20:40 +0200

    Q1

--- Demo/Program.cs
+++ Demo/Program.cs
@@ -4,8 +4,16 @@
     {
         static void Main(string[] args)
         {
-            
-            #region Loop Statment
+            #region Q1
+            Console.Write("Enter an integer: ");
+            int number = int.Parse(Console.ReadLine());
+            for (int i = 1; i <= number; i++) 
+            {
+                Console.Write(i);
+                if (i < number) {
+                    Console.Write("\n"); 
+                } 
+            }
             #endregion
         }
     }
